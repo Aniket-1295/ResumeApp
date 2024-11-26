acb198f411fba7518949ea25c261cc16bf081337
Aniket <aniketkamble1642002@gmail.com>
2024-11-26 09:34:42 +0530

    Added Footer and AboutUs Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Box, Typography, useTheme } from "@mui/material";
+
 
 const Footer = () => {
+
+  const theme = useTheme();
+  const main = theme.palette.primary.main;
   return (
-    <div>Footer</div>
+    // <div>Footer</div>
+    <footer>
+      <Box
+        backgroundColor={theme.palette.background.alt}
+        width="100%"
+        maxWidth="98%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        p="1rem"
+        m="0.5rem auto"
+        borderRadius="8px"
+      >
+        <Typography variant="p" color={main} textAlign="center">
+          <span color="#000">All Rights Reserved ©</span> ResumeBuilder
+        </Typography>
+      </Box>
+    </footer>
   )
 }
 
--- src/scenes/AboutUs.jsx
+++ src/scenes/AboutUs.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
+import { Box, Link, Typography, useTheme, useMediaQuery } from "@mui/material";
+import aboutus from "../assets/aboutus.png";
+import { LinkedIn } from "@mui/icons-material";
+import { ContactMail } from "@mui/icons-material";
+// import { X } from "@mui/icons-material";
+import { Instagram } from "@mui/icons-material";
+
 
 const AboutUs = () => {
+    const theme = useTheme();
+    const primaryMain = theme.palette.primary.main;
+    const isMobileScreen = useMediaQuery("(max-width: 600px)");
   return (
-    <div>AboutUs</div>
+    // <div>AboutUs</div>
+
+    <Box p="1rem" mt="2rem">
+    <Box
+      display="flex"
+      flexDirection={isMobileScreen ? "column-reverse" : "row"}
+      justifyContent="space-between"
+      gap="2rem"
+      borderRadius="8px"
+      p={isMobileScreen ? "2rem 4%" : "4rem 4%"}
+      backgroundColor={theme.palette.background.alt}
+    >
+      <Box width="100%">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(2rem, 4rem, 1.5rem)"
+          mb="2rem"
+        >
+          About Us
+        </Typography>
+        <Typography variant="h5" mb="1rem" color={primaryMain}>
+          Welcome to ResumeBuilder!
+        </Typography>
+        <Typography fontSize="1rem" lineHeight="1.75rem" variant="subtitle">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sunt quas hic similique cumque, quo praesentium voluptatem eos deserunt, explicabo optio sed nam. Praesentium voluptas ipsam expedita pariatur adipisci. Nesciunt quam, aperiam nihil repudiandae sed at laboriosam neque voluptate veritatis.
+          <br /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat ad consequuntur expedita quis aliquid ex numquam nulla soluta voluptatum! Voluptates magni neque fugit expedita porro pariatur dicta recusandae distinctio voluptate animi. Numquam harum possimus esse quia nobis laboriosam labore aliquid!
+        </Typography>
+        <Typography mt="1rem" color={primaryMain}>
+          Find Us :
+        </Typography>
+        <Box
+          mt="1rem"
+          display="flex"
+          justifyContent="space-between"
+          gap="10px"
+          alignContent="center"
+          width="100%"
+          maxWidth="200px"
+        >
+          <Link
+            href="https://www.linkedin.com/in/aniket-kamble-865694214"
+            sx={{ cursor: "pointer" }}
+          >
+            <LinkedIn />
+          </Link>
+         
+          <Link
+            href="https://www.instagram.com/aniket_k313/"
+            sx={{ cursor: "pointer" }}
+          >
+            <Instagram />
+          </Link>
+          <Link
+            href="mailto:aniketkamble1642002@gmail.com"
+            sx={{ cursor: "pointer" }}
+          >
+            <ContactMail />
+          </Link>
+        </Box>
+      </Box>
+      <Box
+        width="100%"
+        maxWidth="600px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <img src={aboutus} width="100%" height="auto" alt="img" />
+      </Box>
+    </Box>
+  </Box>
+
   )
 }
 
